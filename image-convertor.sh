d1233660b70fcf29e2a6e92d4b095d0b5c8d9468
gulfamansari <gulfamansari1515@gmail.com>
2019-08-28 11:44:48 +0530

    update node js code

--- image-convertor.sh
+++ image-convertor.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 #  To Compress images, You need to install pngquant and jpegoptim for .png and .jpeg image optiomization
 
-echo "Please enter the path for unoptimized images like /home/gulfamansari/Pictures/images/";
-read INPUT_IMAGES_FILE_PATH;
-
-# INPUT_IMAGES_FILE_PATH="/home/gulfamansari/Pictures/images/";
+# echo "Please enter the path for unoptimized images like /home/gulfamansari/Pictures/images/";
+# read INPUT_IMAGES_FILE_PATH;
+INPUT_IMAGES_FILE_PATH="/home/gulfamansari/Pictures/images/";
 
 # Compress the PNG files
-find $INPUT_IMAGES_FILE_PATH*.png -exec pngquant --force --quality=40-100 --skip-if-larger --verbose \{} --output \{} \;
+find $INPUT_IMAGES_FILE_PATH*.{png,PNG} -exec pngquant --force --quality=40-100 --skip-if-larger --verbose \{} --output \{} \;
 
 
 # Compres  the jpeg files with the same modification date as original files.
@@ -15,4 +14,4 @@ find $INPUT_IMAGES_FILE_PATH*.png -exec pngquant --force --quality=40-100 --skip
 # -m50 = 50% compression rate
 # -p = preserve the modification time
 # -t = print total after processing the files
-jpegoptim INPUT_IMAGES_FILE_PATH*.{jpg,JPG,jpeg,JPEG} -m50 -o -p -t
+jpegoptim $INPUT_IMAGES_FILE_PATH*.{jpg,JPG,jpeg,JPEG} -m50 -o -p -t
---
+++ index.js
@@ -0,0 +1,99 @@
+var replace = require("replace");
+
+// Things Needs to specify
+const TITLE = ''
+const FOCUS_KEYWORD = '';
+const SRC_BASE_URL = '';
+const SLUG = '';
+
+
+replace({
+    regex: 'alt=""',
+    replacement: FOCUS_KEYWORD,
+    paths: ['template/article.php'],
+    recursive: true,
+    silent: true,
+});
+
+replace({
+    regex: '<h1></h1>',
+    replacement: `<h1>${TITLE}</h1>`,
+    paths: ['template/article.php'],
+    recursive: true,
+    silent: true,
+});
+
+replace({
+    regex: SRC_BASE_URL,
+    replacement: 'images',
+    paths: ['template/article.php'],
+    recursive: true,
+    silent: true,
+});
+
+replace({
+    regex: 'wp-image-',
+    replacement: 'img img-responsive ',
+    paths: ['template/article.php'],
+    recursive: true,
+    silent: true,
+});
+
+replace({
+    regex: '<h2>',
+    replacement: '<h2 class="subHeading-with-border">',
+    paths: ['template/article.php'],
+    recursive: true,
+    silent: true,
+});
+
+replace({
+    regex: '&nbsp;',
+    replacement: '',
+    paths: ['template/article.php'],
+    recursive: true,
+    silent: true,
+});
+
+replace({
+    regex: '</h2>',
+    replacement: '</h2><br>',
+    paths: ['template/article.php'],
+    recursive: true,
+    silent: true,
+});
+
+replace({
+    regex: '<ol>',
+    replacement: '<ol class="order-large-list">',
+    paths: ['template/article.php'],
+    recursive: true,
+    silent: true,
+});
+
+replace({
+    regex: '<ul>',
+    replacement: '<ul class="large-list">',
+    paths: ['template/article.php'],
+    recursive: true,
+    silent: true,
+});
+
+replace({
+    regex: '<li>',
+    replacement: '<li><p>',
+    paths: ['template/article.php'],
+    recursive: true,
+    silent: true,
+});
+
+replace({
+    regex: '</li>',
+    replacement: '</p></li>',
+    paths: ['template/article.php'],
+    recursive: true,
+    silent: true,
+});
+
+
+
