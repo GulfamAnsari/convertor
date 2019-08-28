2eb3a5334a03c6d6627160cd363078aa03dbd3f8
gulfamansari <gulfamansari1515@gmail.com>
2019-08-28 17:53:36 +0530

    change compression ratio

--- image-convertor.sh
+++ image-convertor.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 #  To Compress images, You need to install pngquant and jpegoptim for .png and .jpeg image optiomization
 
-# echo "Please enter the path for unoptimized images like /home/gulfamansari/Pictures/images/";
-# read INPUT_IMAGES_FILE_PATH;
 INPUT_IMAGES_FILE_PATH=$@
 
 echo "#############################################################"
@@ -15,8 +13,8 @@ mogrify -format jpg $INPUT_IMAGES_FILE_PATH'*.png'
 # remove all png
 sudo rm -rf -d $INPUT_IMAGES_FILE_PATH*.png
 
-# Resize 50%
-mogrify -resize 50% $INPUT_IMAGES_FILE_PATH'*.jpg'
+# Resize 100-90%
+mogrify -resize 90% $INPUT_IMAGES_FILE_PATH'*.jpg'
 
 # Compress the PNG files
 find $INPUT_IMAGES_FILE_PATH -regex '.*\.\(PNG\|png\)' -exec pngquant --force --quality=30-60 --skip-if-larger --verbose \{} --output \{} \;
@@ -24,7 +22,7 @@ find $INPUT_IMAGES_FILE_PATH -regex '.*\.\(PNG\|png\)' -exec pngquant --force --
 
 # Compres the jpeg files with the same modification date as original files.
 # -o = overwrite
-# -m50 = 50% compression rate
+# -m80 = 80% compression rate
 # -p = preserve the modification time
 # -t = print total after processing the files
-jpegoptim $INPUT_IMAGES_FILE_PATH* -m35 -o -p -t
+jpegoptim $INPUT_IMAGES_FILE_PATH* -m80 -o -p -t
