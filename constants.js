856f82db58cc4c6e554b7e42350ff291fc794708
gulfamansari <gulfamansari1515@gmail.com>
2021-06-05 20:32:57 +0530

    a

--- constants.js
+++ constants.js
@@ -1,13 +1,12 @@
-var CONSTANTS = require('./cred');
 module.exports = {
-    USERNAME: CONSTANTS.USERNAME,
-    PASSWORD: CONSTANTS.PASSWORD,
     FOCUS_KEYWORD: 'google drive vs dropbox vs onedrive which one is better',
     IMAGES_WEBPAGE_URL: 'https://droidtechknow.000webhostapp.com/2021/06/online-doctor-consultation-made-easy-with-five-best-apps',
     AUTHOR: 'Reeya',
     ARTICLE_DATA: 'May 06, 2021',
     CATAGORY: 'differences',
     SUBCATAGORY: '',
+    USERNAME: process.env.SERVERNAME,
+    PASSWORD: process.env.PASSWORD,
     ANDROID_DOWNLOAD: 'Get it on PlayStore',
     IOS_DOWNLOAD: 'Get it on AppStore',
     FLIPKART_BUY: '',
